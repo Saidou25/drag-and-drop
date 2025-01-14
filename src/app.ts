a0743143add72ddbaa2b145452727d0861080d70
Saidou25 <115666389+Saidou25@users.noreply.github.com>
2025-01-14 14:11:07 -0500

    Displaying the form in the dom using class and constructor

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,29 @@
-const greet = (name: string) => {
-console.log(`Hello ${name}.`);
+class ProjectForm {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLFormElement;
+
+  constructor() {
+    // Select elements from the DOM and assign them to the properties
+    this.templateElement = document.getElementById(
+      "project-input"
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+
+    // Import the HTML element using innerHTML
+    const templateContent = this.templateElement.innerHTML;
+    const wrapper = document.createElement("div");
+    wrapper.innerHTML = templateContent;
+
+    // Extract the form element from the wrapper. The firstElementChild of the wrapper is used to retrieve the form element.
+    this.element = wrapper.firstElementChild as HTMLFormElement;
+    //   The attach method inserts the element into the hostElement (app div)
+    this.attach();
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement("afterbegin", this.element);
+  }
 }
-greet("Saidou Monta");
-greet("Mr Sy");
+
+const prj = new ProjectForm();
