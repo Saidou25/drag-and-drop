69546db56aefd1970fcc72280e6f3cc067c2f6be
Saidou25 <115666389+Saidou25@users.noreply.github.com>
2025-01-14 16:09:06 -0500

    Worked on interacting with the dom in fact gathering user inputs. Added some validation and everything works find

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,86 @@
 class ProjectForm {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
-  element: HTMLFormElement;
+  formElement: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLTextAreaElement;
+  peopleInputElement: HTMLInputElement;
 
   constructor() {
-    // Select elements from the DOM and assign them to the properties
+    // Select template and host elements
     this.templateElement = document.getElementById(
       "project-input"
     )! as HTMLTemplateElement;
     this.hostElement = document.getElementById("app")! as HTMLDivElement;
 
-    // Import the HTML element using innerHTML
-    const templateContent = this.templateElement.innerHTML;
-    const wrapper = document.createElement("div");
-    wrapper.innerHTML = templateContent;
+    // Import the content of the template
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    this.formElement = importedNode.firstElementChild as HTMLFormElement;
+    this.formElement.id = "user-input";
 
-    // Extract the form element from the wrapper. The firstElementChild of the wrapper is used to retrieve the form element.
-    this.element = wrapper.firstElementChild as HTMLFormElement;
-    //   The attach method inserts the element into the hostElement (app div)
+    // Access input elements after they are part of the form
+    this.titleInputElement = this.formElement.querySelector(
+      "#title"
+    ) as HTMLInputElement;
+    this.descriptionInputElement = this.formElement.querySelector(
+      "#description"
+    ) as HTMLTextAreaElement;
+    this.peopleInputElement = this.formElement.querySelector(
+      "#people"
+    ) as HTMLInputElement;
+
+    this.configureForm();
     this.attach();
   }
 
+  private gatherUserInput(): [string, string, number] | void {
+    const titleData = this.titleInputElement.value.trim();
+    const descriptionData = this.descriptionInputElement.value.trim();
+    const peopleData = this.peopleInputElement.value.trim();
+
+    if (
+      titleData.trim().length === 0 ||
+      descriptionData.trim().length === 0 ||
+      peopleData.trim().length === 0
+    ) {
+      alert("Invalid input, please try again!");
+      return;
+    } else {
+      console.log(
+        `Title: ${titleData}, Description: ${descriptionData}, People: ${peopleData}`
+      );
+      return [titleData, descriptionData, +peopleData];
+    }
+  }
+
+  private clerarInputs = () => {
+    this.titleInputElement.value = "";
+    this.descriptionInputElement.value = "";
+    this.peopleInputElement.value = "";
+  };
+
+  private handleSubmit(event: Event) {
+    event.preventDefault();
+
+    const userInput = this.gatherUserInput();
+    if (Array.isArray(userInput)) {
+      const [titleData, descriptionData, peopleData] = userInput;
+      console.log(
+        `Title: ${titleData}, Description: ${descriptionData}, People: ${peopleData}`
+      );
+      this.clerarInputs();
+    }
+  }
+
+  private configureForm() {
+    this.formElement.addEventListener("submit", this.handleSubmit.bind(this));
+  }
+
   private attach() {
-    this.hostElement.insertAdjacentElement("afterbegin", this.element);
+    this.hostElement.insertAdjacentElement("afterbegin", this.formElement);
   }
 }
 
